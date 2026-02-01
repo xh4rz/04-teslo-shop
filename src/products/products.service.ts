281c9e1811ecc4fe776bd298d9fb0c14d2e2a597
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-02-01 18:23:18 -0500

    129. Paginar en TypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,6 +10,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -32,8 +33,13 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRepository.find({});
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
   async findOne(id: string) {
