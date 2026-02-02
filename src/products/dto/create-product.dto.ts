771e451ef63824c022dabd94c61ab372f336cd13
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-02-01 21:53:23 -0500

    134. Nueva columna - Tags

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -40,4 +40,11 @@ export class CreateProductDto {
 
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
+
+  @IsString({
+    each: true,
+  })
+  @IsArray()
+  @IsOptional()
+  tags?: string[];
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -44,7 +44,12 @@ export class Product {
 
   @Column('text')
   gender: string;
-  // tags
+
+  @Column('text', {
+    array: true,
+    default: [],
+  })
+  tags: string[];
   // images
 
   @BeforeInsert()
