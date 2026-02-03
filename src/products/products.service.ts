71e4825d552e408fd801a22ecbabbc4fc0a685d5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-02-03 01:57:26 -0500

    146. Eliminación en cascada

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -154,4 +154,14 @@ export class ProductsService {
       'Unexpected error, check server logs',
     );
   }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query.delete().where({}).execute();
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
 }
