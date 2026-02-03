df556408215b0dbfb4f7514b147751526ec561f7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-02-03 01:47:41 -0500

    145. Transacciones

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -111,10 +111,30 @@ export class ProductsService {
     // create query runner
     const queryRunner = this.dataSource.createQueryRunner();
 
+    await queryRunner.connect();
+
+    await queryRunner.startTransaction();
+
     try {
-      await this.productRepository.save(product);
-      return product;
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+
+        product.images = images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        );
+      }
+
+      await queryRunner.manager.save(product);
+
+      await queryRunner.commitTransaction();
+
+      await queryRunner.release();
+      // await this.productRepository.save(product);
+
+      return this.findOnePlain(id);
     } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
       this.handleDBExceptions(error);
     }
   }
