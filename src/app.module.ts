45f66de7d780bb9e1260a54a80e9c5c510e73ad9
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-02-04 21:39:43 -0500

    160. Otras formas de desplegar archivos 161. Colocar imágenes en el directorio estático

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -18,6 +20,9 @@ import { FilesModule } from './files/files.module';
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     ProductsModule,
     CommonModule,
