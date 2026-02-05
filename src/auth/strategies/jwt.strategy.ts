d5556ac8133afbb9a0206beaae4b10bac93c5315
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-02-05 01:51:03 -0500

    173. JwtStrategy - Parte 2

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,37 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET') as string,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
   async validate(payload: JwtPayload): Promise<User> {
     const { email } = payload;
 
-    return;
+    const user = await this.userRepository.findOneBy({
+      email,
+    });
+
+    if (!user) throw new UnauthorizedException('Token not valid');
+
+    if (!user.isActive)
+      throw new UnauthorizedException('User is inactive, talk with an admin');
+
+    return user;
   }
 }
