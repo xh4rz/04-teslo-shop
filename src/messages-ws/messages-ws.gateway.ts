b08411883880a9691770f780aa2162e53ed36702
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-07-04 23:46:22 -0500

    208. Cliente - Clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,27 +2,34 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     // console.log('Cliente conectado:', client.id);
     this.messagesWsService.registerClient(client);
-    console.log({ conectados: this.messagesWsService.getConnectedClients() });
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
     // console.log('Cliente desconectado:', client.id);
     this.messagesWsService.removeClient(client.id);
-    console.log({
-      conectados: this.messagesWsService.getConnectedClients(),
-    });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 }
