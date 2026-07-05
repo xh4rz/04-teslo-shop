4421e2b417c78d9807dc68de25ad50acebe06fe8
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-07-04 22:43:57 -0500

    206. Server - Mantener identificados los clientes

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -13,10 +13,16 @@ export class MessagesWsGateway
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
-    console.log('Cliente conectado:', client.id);
+    // console.log('Cliente conectado:', client.id);
+    this.messagesWsService.registerClient(client);
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado:', client.id);
+    // console.log('Cliente desconectado:', client.id);
+    this.messagesWsService.removeClient(client.id);
+    console.log({
+      conectados: this.messagesWsService.getConnectedClients(),
+    });
   }
 }
