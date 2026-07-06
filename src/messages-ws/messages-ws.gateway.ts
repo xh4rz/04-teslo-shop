0ae4fd19f22cb5c7c8154436631b76bbf7498972
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-07-06 04:41:45 -0500

    213. Enlazar Socket con Usuario

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -22,21 +22,20 @@ export class MessagesWsGateway
     private readonly jwtService: JwtService,
   ) {}
 
-  handleConnection(client: Socket) {
+  async handleConnection(client: Socket) {
     const token = client.handshake.headers.authentication as string;
 
     let payload: JwtPayload;
 
     try {
       payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
     } catch (error) {
       client.disconnect();
       return;
     }
 
-    console.log({ payload });
-
-    this.messagesWsService.registerClient(client);
+    // console.log({ payload });
 
     this.wss.emit(
       'clients-updated',
@@ -69,7 +68,7 @@ export class MessagesWsGateway
 
     // ! emite a todos
     this.wss.emit('message-from-server', {
-      fullName: 'Soy yo!',
+      fullName: this.messagesWsService.getUserFullName(client.id),
       message: payload.message || 'no-message!!',
     });
   }
